b1667833328901fb4a201ed1743458fe1de97834
Manu Garcia <info@developia.info>
2018-10-07 12:51:56 +0200

    Enable Movies microservice to be discovered

--- movies/src/main/java/info/developia/showltan/movies/MoviesApplication.java
+++ movies/src/main/java/info/developia/showltan/movies/MoviesApplication.java
@@ -2,7 +2,9 @@ package info.developia.showltan.movies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MoviesApplication {
 
