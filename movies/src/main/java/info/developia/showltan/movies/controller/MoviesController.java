f89bbb1cd377aa26e16118ca9a286b82a11ae5d4
Manu Garcia <manu.garcia@accenture.com>
2018-10-12 01:09:24 +0200

    Update movies service with hystrix and some hardcored data

--- movies/src/main/java/info/developia/showltan/movies/controller/MoviesController.java
+++ movies/src/main/java/info/developia/showltan/movies/controller/MoviesController.java
@@ -1,31 +1,28 @@
 package info.developia.showltan.movies.controller;
 
+import info.developia.showltan.movies.model.Movies;
+import info.developia.showltan.movies.service.MoviesService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashSet;
 import java.util.Set;
 
 @RestController
 @RequestMapping("/movies")
 public class MoviesController {
 
+    @Autowired
+    private MoviesService moviesService;
+
     @GetMapping("/all")
-    ResponseEntity<Set<String>> getAll() {
-        Set<String> movies = new HashSet<>();
-
-        movies.add("Reservoir Dogs");
-        movies.add("Pulp Fiction");
-        movies.add("Jackie Brown");
-        movies.add("Death Proof");
-        movies.add("Django");
-        movies.add("Inglorious Basterds");
-        movies.add("The Hateful Eight");
-
-        return new ResponseEntity<>(movies, HttpStatus.OK);
+    ResponseEntity<Set<Movies>> getAll() {
+        return moviesService.getLast()
+                .map(m -> new ResponseEntity<>(m, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NO_CONTENT));
     }
 
 }
