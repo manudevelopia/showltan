f91608b5834dec5e807ecc3e6db9a98ae8329b42
manudevelopia <manugarciarodriguez@gmail.com>
2018-10-22 18:53:09 +0200

    Remove unused repository endpoint

--- tvshows/src/main/java/info/developia/showltan/tvshows/repository/TvShowRepository.java
+++ tvshows/src/main/java/info/developia/showltan/tvshows/repository/TvShowRepository.java
@@ -4,12 +4,7 @@ import info.developia.showltan.tvshows.model.TvShow;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-import java.util.Set;
-
 @Repository
 public interface TvShowRepository extends JpaRepository<TvShow, String> {
-
-    Optional<Set<TvShow>> findByTags(Set<String> tags);
 
 }
